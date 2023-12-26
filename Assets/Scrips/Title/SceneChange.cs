a6699bd6360b79cf37c338505db7aaf3d21c014a
answnsgh99 <147613446+answnsgh99@users.noreply.github.com>
2023-12-26 21:48:48 +0900

    12/26

---
+++ Assets/Scrips/Title/SceneChange.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    [SerializeField] private GameObject ob;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Change()
+    {
+        DontDestroyOnLoad(ob);
+        SceneManager.LoadScene("Game");
+        
+    }
+}
